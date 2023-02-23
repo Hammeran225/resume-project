0accdf91ca53ff716a22dc5d93881e92ccb2f9d1
Hammeran225 <57191815+Hammeran225@users.noreply.github.com>
2023-02-23 10:19:08 -0600

    test 1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,44 @@
-// JS scripts placed here
+// Smooth scrolling effect
+$('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if (target.length) {
+      event.preventDefault();
+      $('html, body').stop().animate({
+        scrollTop: target.offset().top
+      }, 1000);
+    }
+  });
+  
+  // Dropdown navigation functionality
+  $('.dropdown').click(function() {
+    $('.dropdown-menu').toggleClass('show');
+  });
+  
+  // Slideshow for clothing items
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+  }
+  
